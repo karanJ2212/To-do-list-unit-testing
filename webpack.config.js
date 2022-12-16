df1d86a0b37a08ace9f22bdb24401e0d6c0605d3
rbhatt199924 <rbhatt199924@gmail.com>
2022-12-16 18:03:19 +0530

    fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
