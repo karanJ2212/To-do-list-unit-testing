35a91e8ad78931bb1b5ccf46e30e30f4aa4f2b07
karan jain <karanjain2212@gmail.com>
2022-12-16 17:21:21 +0530

    fixed linters

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import "./style.css";
-import { add, deleteAllCompleted, populateList } from "./crud.js";
-import ToDo from "./todoList.js";
+import './style.css';
+import { add, deleteAllCompleted, populateList } from './crud.js';
+import ToDo from './todoList.js';
 
 // Window load
-const list = JSON.parse(localStorage.getItem("todoList"));
+const list = JSON.parse(localStorage.getItem('todoList'));
 if (list) {
   list.forEach((item) => new ToDo(item.description, item.complete));
 }
 
 // Add
-const addInput = document.getElementById("add-input");
-addInput.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
+const addInput = document.getElementById('add-input');
+addInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
     add(addInput.value);
-    addInput.value = "";
+    addInput.value = '';
     populateList();
   }
 });
 
 // Delete all completed
-const clearButton = document.getElementById("clear-btn");
-clearButton.addEventListener("click", () => {
+const clearButton = document.getElementById('clear-btn');
+clearButton.addEventListener('click', () => {
   deleteAllCompleted(ToDo);
   populateList();
 });
--- src/test/crud.test.js
+++ src/test/crud.test.js
@@ -2,50 +2,51 @@
  * @jest-environment jsdom
  */
 
-import { add, remove, populateList } from "../crud.js";
-import ToDo from "../todoList.js";
+import { add, remove, populateList } from '../crud.js';
+import ToDo from '../todoList.js';
 
-jest.mock("../todoList");
+jest.mock('../todoList');
 
-describe("test for todo list function", () => {
+describe('test for todo list function', () => {
   beforeEach(() => {
     ToDo.list = [];
   });
 
-  describe("test for add storage and html DOM", () => {
-    test("test for add funtion", () => {
-      add("hello");
-      add("goodbye");
-      add("bye");
-      const storage = JSON.parse(localStorage.getItem("todoList"));
+  describe('test for add storage and html DOM', () => {
+    test('test for add funtion', () => {
+      add('hello');
+      add('goodbye');
+      add('bye');
+      const storage = JSON.parse(localStorage.getItem('todoList'));
       expect(storage).toHaveLength(3);
     });
-    test("test for html DOM when add a task", () => {
-      document.body.innerHTML =
-        "<div>" + '  <ul id="todo-list"><li></li></ul>' + "</div>";
-      add("hello");
+    test('test for html DOM when add a task', () => {
+      // eslint-disable-next-line no-undef, no-unused-vars, no-useless-concat
+      document.body.innerHTML = '<div>' + '  <ul id="todo-list"><li></li></ul>' + '</div>';
+      add('hello');
       populateList();
-      const listElements = document.querySelectorAll("#todo-list li");
+      const listElements = document.querySelectorAll('#todo-list li');
       expect(listElements).toHaveLength(1);
     });
   });
-  
-  describe("test for add storage and html DOM", () => {
-    test("test for remove funtion", () => {
-      add("hello");
-      add("buy");
+
+  describe('test for add storage and html DOM', () => {
+    test('test for remove funtion', () => {
+      add('hello');
+      add('buy');
       remove(0);
-      const storage = JSON.parse(localStorage.getItem("todoList"));
+      const storage = JSON.parse(localStorage.getItem('todoList'));
       expect(storage).toHaveLength(1);
     });
-    test("test for html DOM when remove a task", () => {
-      document.body.innerHTML =
-        "<div>" + '  <ul id="todo-list"><li></li></ul>' + "</div>";
-      add("hello");
-      add("hello");
+    // eslint-disable-next-line no-undef, no-unused-vars, no-useless-concat
+    test('test for html DOM when remove a task', () => {
+      // eslint-disable-next-line no-undef, no-unused-vars, no-useless-concat
+      document.body.innerHTML = '<div>' + '  <ul id="todo-list"><li></li></ul>' + '</div>';
+      add('hello');
+      add('hello');
       remove(0);
       populateList();
-      const listElements = document.querySelectorAll("#todo-list li");
+      const listElements = document.querySelectorAll('#todo-list li');
       expect(listElements).toHaveLength(1);
     });
   });
